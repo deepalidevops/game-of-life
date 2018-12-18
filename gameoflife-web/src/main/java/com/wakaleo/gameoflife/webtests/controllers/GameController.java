d1f70ef41fefc9c28bf9ec8deff037d57effcc8c
deepalidevops <45748139+deepalidevops@users.noreply.github.com>
2018-12-18 10:05:22 +0530

    intentional break

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.wakaleo.gameoflife.domain.Universe;
+imports com.wakaleo.gameoflife.domain.Universe;
 
  /**
  * Wakaleo Consulting - John Ferguson Smart
